b50f60c700e09278a34fc3f5d274a28d55a781af
Dharsan <dharsansrinivasan2004@gmail.com>
2023-12-27 12:53:14 +0530

    added edit and update routes

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -12,13 +12,34 @@ class ArticleController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    book = params[:article][:bookName]
+    author = params[:article][:authorName]
+    id = params[:id]
+
+    @article = Article.find(id)
+
+    if @article.update(BookName:book,Author:author)
+      redirect_to "/article"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+
+
+
+  end
+
   def create
     bookName = params[:bookName]
     author = params[:authorName]
     @article = Article.new(BookName:bookName,Author:author)
 
     if @article.save
-        redirect_to article_path(@article)
+        redirect_to article_path(@article) #get req for Show
     else
       render :new, status: :unprocessable_entity
     end
