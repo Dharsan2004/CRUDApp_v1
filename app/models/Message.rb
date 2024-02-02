abbc46f50f246bad144d4068911729692e763413
Dharsan <dharsansrinivasan2004@gmail.com>
2024-02-02 19:14:25 +0530

    Added exception in joke api

--- app/models/Message.rb
+++ app/models/Message.rb
@@ -4,18 +4,29 @@ class Message < ApplicationRecord
 
   def self.getMessage
     url = "https://api.api-ninjas.com/v1/dadjokes?limit=1"
-    response = HTTParty.get(
-      url,
-      headers: {
-        'Content-Type' => 'application/json',
-        'X-Api-Key' => 'moXQ4RF/iAaalLIPT81hMg==ESPDt89BksLFHVrv'
-        # Add other headers as needed
-      }
-    )
 
-    data = JSON.parse(response.body)
-    puts response.body
-    return data[0]["joke"]
+
+
+    begin
+      response = HTTParty.get(
+        url,
+        headers: {
+          'Content-Type' => 'application/json',
+          'X-Api-Key' => 'moXQ4RF/iAaalLIPT81hMg==ESPDt89BksLFHVrv'
+          # Add other headers as needed
+        }
+      )
+
+
+      data = JSON.parse(response.body)
+      p response.body
+      return data[0]["joke"]
+
+      rescue StandardError => e
+        return "Not working Joke API"
+    end
+
+
   end
 
 end
